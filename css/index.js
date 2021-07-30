33c5adaa54e87cf70d7f50c77580022509bb7372
Kromler123 <5926389037@ukr.net>
2021-07-30 19:32:31 +0300

    init commit

---
+++ css/index.js
@@ -0,0 +1,31 @@
+/*let number = 100;
+number = 61;
+console.log(number);
+
+//const 
+const number2 = 50;
+//number2 = 60;
+console.log(number2);
+ 
+const a = 40;
+const b = 23;
+
+let resut = a * b;
+console.log(resut);
+ 
+let resut2 = a - b;
+resut2 = a / b;
+console.log(resut2)
+
+const t = 'Володя';
+      m = 'Ломаковський';
+let resut3 = t +' '+ m;
+console.log(resut3);*/
+let candis = 40;
+while(candis > 0){
+      console.log('Я ВІДКРИВ КОРОБКУ,Я ЗЇВ ЦУКЕРКУ,ЗАКРИВ КОРОБКУ');
+      candis = candis - 1;
+
+}
+
+
